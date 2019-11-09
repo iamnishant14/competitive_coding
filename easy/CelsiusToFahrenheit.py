7639062c14d8cede51e0287705d6e6a0bfde673d
iamnishant <nishantnks@hotmail.com>
2019-11-09 13:29:29 +0530

    easy: [A] Celsius to Fahrenheit conversion.
    Signed-off-by: iamnishant <nishantnks@hotmail.com>

---
+++ easy/CelsiusToFahrenheit.py
@@ -0,0 +1,30 @@
+"""
+Author: Nishant
+
+Problem : Absolute value of the integer.
+
+Date : 09/11/2019
+"""
+import sys
+
+
+class CelsiusToFahrenheit:
+    def __init__(self, celsius):
+        self.__celsius = celsius
+        self.__fahreheit = sys.maxsize
+
+    def celsius_to_fahreheit(self):
+        self.__fahreheit = ((9 * self.__celsius) / 5) + 32
+
+    def get_fahrenheit(self):
+        return self.__fahreheit
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while t > 0:
+        celsius = int(input())
+        c2f = CelsiusToFahrenheit(celsius)
+        c2f.celsius_to_fahreheit()
+        print(c2f.get_fahrenheit())
+        t -= 1
